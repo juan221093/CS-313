d517e3bf7102ae0a27e6bb791a01f174c8e7f5cc
Juan Alvarez <juanitoalvarez221093@gmail.com>
2019-05-27 23:56:45 -0600

    First Connection Test

---
+++ web/cs313/Week05/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+     
+    <link rel="stylesheet" href="pageStyle.css">
+    <title> Juan Alvarez's Assignments </title>
+
+    <style>
+  
+        /* document level styles */
+             body {background-color: #ccdfc8}
+             h1 {color: #656868; font-style: italic; text-align: center;}
+             a {color:ghostwhite}
+             h2 {color: #656868; font-style: italic; text-align: center}
+      
+      </style>
+    </head>
+    
+  <body>
+
+  <?php include 'webPhp.php';?>
+
+      <ul>
+          <li><a class="menu" href="presentationPage.php">Home</a></li>
+          <li><a href="index.php">CS 313 Assignments</a></li>
+        </ul>
+
+      <hr>
+      <img src="logo.png" alt="Logo" class="logo" >
+      <h1>Ecu-World Travel Agency</h1>
+     <blockquote style= "color: #92a39a; font-style: italic; text-align: center; font-family: Garamond;">
+“Man cannot discover new oceans unless he has the courage to lose sight of the shore.” – Andre Gide
+</blockquote>
+     <hr>
+    
+    <h1> CS 313 Assignments </h1>
+      
+    <ul style="text-align:center; list-style:none;"> 
+        <li><a href="presentationPage.php">Week 02</a></li>
+        <li><a href="week03/week03.html">Week 03</a></li>
+        <li><a href="week04/week04.html">Week 04</a></li>
+        <li><a href="week05/week05.html">Week 05</a></li>
+        <li><a href="week06/week06.html">Week 06</a></li>
+        <li><a href="week07/week07.html">Week 07</a></li>
+        <li><a href="week08/week08.html">Week 08</a></li>
+        <li><a href="week09/week09.html">Week 09</a></li>
+        <li><a href="week10/week10.html">Week 10</a></li>
+        <li><a href="week11/week11.html">Week 11</a></li>
+        <li><a href="week12/week12.html">Week 12</a></li>
+        <li><a href="week13/week13.html">Week 13</a></li>
+    </ul>
+
+    <hr>
+       
+    <footer>
+      <img src="footer.jpg" alt="footer" class="footer" >
+    </footer>
+
+  </body>
+</html>
---
+++ web/cs313/Week05/webPhp.php
@@ -0,0 +1,6 @@
+<!-- Made by Juan Alvarez -->
+
+<?php
+date_default_timezone_set("America/Boise");
+echo "The time is " . date("h:i:sa");
+?>
