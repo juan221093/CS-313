27f7fc659b5356341b339e8f0eac438475f08aa0
Juan Alvarez <juanitoalvarez221093@gmail.com>
2019-06-10 12:16:05 -0600

    Testing Code

--- web/cs313/Week05/dBconnect.php
+++ web/cs313/Week05/dBconnect.php
@@ -7,15 +7,16 @@ $db = NULL;
 
 try
 {
-  $dbUrl = getenv('DATABASE_URL');
+  $dbUrl = getenv('postgres://cllstbfnqmjbse:c4681ab5380bc2b0c699c714480be54594ab059fcebcc00953699fb4342c82e7@ec2-23-21-186-85.compute-1.amazonaws.com:5432/d4uj8uaup6ucv9
+  ');
 
   $dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+  $dbHost = $dbOpts["ec2-23-21-186-85.compute-1.amazonaws.com"];
+  $dbPort = $dbOpts["5432"];
+  $dbUser = $dbOpts["cllstbfnqmjbse"];
+  $dbPassword = $dbOpts["c4681ab5380bc2b0c699c714480be54594ab059fcebcc00953699fb4342c82e7"];
+  $dbName = ltrim($dbOpts["d4uj8uaup6ucv9"],'/');
 
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
