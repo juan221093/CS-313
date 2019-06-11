d696d3ab47f103a109d1261f78ef52cabb693c31
Juan Alvarez <juanitoalvarez221093@gmail.com>
2019-06-11 01:08:28 -0600

    Testing Code

---
+++ web/cs313/Week05/projectDB.sql
@@ -0,0 +1,17 @@
+INSERT INTO packages (city,num_days,num_nights,num_people,total_price, package_id)
+    values ('Santa Cruz',5,4,2, 900, 1);
+
+INSERT INTO packages (city,num_days,num_nights,num_people,total_price, package_id)
+    values ('Santa Cruz',4,3,2, 750, 2);
+
+
+INSERT INTO packages (city,num_days,num_nights,num_people,total_price, package_id)
+    values ('Isabela',5,4,2, 1200, 3);
+
+
+
+INSERT INTO packages (city,num_days,num_nights,num_people,total_price, package_id)
+    values ('Isabela',4,3,2,1000, 4); 
+	
+	
+	SELECT * FROM packages;
