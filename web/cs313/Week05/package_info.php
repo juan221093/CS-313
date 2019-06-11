4d489a596b1f520d6fafe560b64cbd953d5c5a29
Juan Alvarez <juanitoalvarez221093@gmail.com>
2019-06-11 00:41:52 -0600

    Testing Code

--- web/cs313/Week05/package_info.php
+++ web/cs313/Week05/package_info.php
@@ -5,6 +5,7 @@ if (!isset($_GET['package_id']))
 
 }
 $city = htmlspecialchars($_GET['package_id']);
+$days = htmlspecialchars($_GET['days_id']);
 
 ?>
 
@@ -38,7 +39,8 @@ $city = htmlspecialchars($_GET['package_id']);
 “Man cannot discover new oceans unless he has the courage to lose sight of the shore.” – Andre Gide
 </blockquote>
        <hr> 
-       <h2>Package Info For City <?php echo $city ;?></h2>
+       <h2>Package Info For City <?php echo $city ;?> <?php echo $days;?> </h2>
+
        <div class="introduction"> 
        
 
